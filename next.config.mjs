645680cadb9789804c2d88eafc036659ed8c7621
saramsh <stha.saramsh.mdit@gmail.com>
2024-08-29 16:43:06 +0545

    base

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), "styles")],
+  },
+  experimental: {
+    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+  },
+};
 
 export default nextConfig;
