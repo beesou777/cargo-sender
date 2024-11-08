099d016b5181601b04fc3393aec01e0fb62b7bc4
beesou777 <sbeesou@gmail.com>
2024-11-08 17:18:03 +0545

    working : order create page

---
+++ src/app/dashboard/orders/new/components/new_order.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+
+export default function NewOrder() {
+    return (
+        <div className="text-[#fff]">new order</div>
+    )
+}
--- src/app/dashboard/orders/new/page.tsx
+++ src/app/dashboard/orders/new/page.tsx
@@ -1,11 +1,26 @@
 "use client";
+import { Tabs, Title } from '@mantine/core';
 import React from 'react'
 const DashboardPage = () => {
     return (
         <>
-        <h1>this is orders new</h1>
+        <Title className='h3 p-[10px_0px]'>New Order</Title>
+        <Tabs defaultValue={'new_order'}>
+            <Tabs.List>
+                <Tabs.Tab value={'new_order'}>New Order</Tabs.Tab>
+                <Tabs.Tab value={'batch_import'}>Batch Import</Tabs.Tab>
+                <Tabs.Tab value={'public_api'}>Public API</Tabs.Tab>
+            </Tabs.List>
+            <Tabs.Panel value={'orders'}>
+                {/* <Summary order= {order} /> */}
+                ksjhf
+            </Tabs.Panel>
+            {/* <Tabs.Panel value={'document'}>
+                <DocumentTable data={dashboardData} />
+            </Tabs.Panel> */}
+        </Tabs>
         </>
-    )
+);
 }
 
 export default DashboardPage
